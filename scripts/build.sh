2a1b14cd4380815031c5b520978018a4ceae29d2
Yehor Konoval <ekonoval@gmail.com>
2018-01-08 15:13:05 +0200

    build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,20 @@
-# QA
-eslint js/source js/__tests__
-flow
-npm test
+#!/usr/bin/env bash
 # js transform
-babel js/source/ -d js/build
+echo "^^ statted at "; date;
+
+babel --presets react,es2015,stage-0 js/source/ -d js/build
+echo "---->>> babel done"; date;
+
 # js package
 browserify js/build/app.js -o bundle.js
+echo "---->>> app.js done"; date;
+
 browserify js/build/discover.js -o discover-bundle.js
+
+echo "---->>> discover.js done"; date;
+
 # css package
 cat css/*/* css/*.css | sed 's/..\/..\/images/images/g' > bundle.css
 # done
-date; echo;
+echo "------------------ done ------------------";
 
--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,1 +1,1 @@
-watch "sh scripts/build.sh" js/source/ js/__tests__/ css/
+watch "sh scripts/build.sh" js/source/ css/
